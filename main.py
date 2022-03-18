ffd84fe397867b60759e4e26d100dc5efa2b4857
lucaspires-source <pires.lucas94@gmail.com>
2022-03-18 13:30:50 -0300

    Using construct

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 class Item:
-    def calculate_total_price(self, x, y):
-        return x * y
+    pay_rate = 0.8  # the pay rate after 20% discount
+    all = []
+    def __init__(self, name: str, price: float, quantity=0):
+        # Run validations to the received arguments
+        assert price >= 0, f"Prince {price} is not greater than zero"
+        assert quantity >= 0, f"Prince {quantity} is not greater or equal to zero"
+
+        # Assign to sel object
+        self.name = name
+        self.price = price
+        self.quantity = quantity
+
+        # Actions to execute
+        Item.all.append(self)
+    def calculate_total_price(self):
+        return self.price * self.quantity
+
+    def apply_discount(self):
+        self.price = self.price * self.pay_rate
+
+    def __repr__(self):
+        return f"Item('{self.name}','{self.price}',{self.quantity})"
 
 
+item1 = Item('Phone', 100, 1)
+item2 = Item('Laptop', 1000, 5)
+item3 = Item('Cable', 10, 5)
+item4 = Item('Mouse', 50, 5)
+item5 = Item('Keyboard', 75, 5)
 
-item1 = Item()
-item1.name = 'Phone'
-item1.price = 100
-item1.quantity = 5
-
-
-item2 = Item()
-item2.name = 'Laptop'
-item2.price = 1000
-item2.quantity = 3
-
-print(item2.calculate_total_price(item2.price, item2.quantity))
+print(Item.all)
